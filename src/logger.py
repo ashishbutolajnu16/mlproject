d8dc24b04fa3de5bb05cbdc133a1d65060445f03
ashishbutolajnu16 <ashishbutola7@gmail.com>
2026-03-11 08:35:11 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,18 +1,25 @@
-import logging
-import os
-from datetime import datetime
+import sys
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs")
-os.makedirs(logs_path, exist_ok=True)
-LOGS_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+from src.logger import logging
 
 
-logging.basicConfig(
-    filename=LOGS_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
+def error_message_details(error, error_details:sys):
+    _,_,exec_tb=error_details.exc_info()
+    file_name = exec_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message is [{2}]".format(file_name, exec_tb.tb_lineno, str(error))
+    return error_message
 
+class CustomException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_details)
+    
+    def __str__(self):
+        return self.error_message
+    
 # if __name__ == "__main__":
-#     logging.info("Logging has started")
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Exception raised")
+#         raise CustomException(e, sys)
